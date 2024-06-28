c3230eb52e99b3f50daad19d9554e38a89331193
Solya <catsubrosa@gmail.com>
2024-06-28 17:34:17 +0300

    Add adaptive for why-ama until tablet

--- src/js/why-ama-slider.js
+++ src/js/why-ama-slider.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // Config for slider to know how many slides show
     const sliderConfig = {
         breakpoints: [
             { maxWidth: 600, minSlides: 2, slidesToShow: 1 },
@@ -9,25 +10,29 @@ $(document).ready(function(){
 
     function getSliderSettings() {
         const slider = $('.why-ama__content');
+        // Find how many blocks are there
         const slides = slider.find('.why-ama__block').length;
         const screenWidth = window.innerWidth;
 
+        // Default values if there one slide
         let settings = {
             arrows: false,
             dots: false,
             infinite: true,
             speed: 300,
             slidesToShow: 1,
-            // autoplay: true,
-            // autoplaySpeed: 10000,
+            autoplay: true,
+            autoplaySpeed: 10000,
             slidesToScroll: 1,
             dotsClass: 'why-ama__dots'
         };
 
+        // Loop for finding correct config
         for (let i = 0; i < sliderConfig.breakpoints.length; i++) {
             const bp = sliderConfig.breakpoints[i];
             if (screenWidth <= bp.maxWidth) {
                 settings.dots = slides >= bp.minSlides;
+                // Change default amount of slides to show
                 settings.slidesToShow = bp.slidesToShow;
                 break;
             }
@@ -36,6 +41,7 @@ $(document).ready(function(){
         return settings;
     }
 
+    // Initializing with settings
     function initializeSlider() {
         const slider = $('.why-ama__content');
         slider.slick(getSliderSettings());
